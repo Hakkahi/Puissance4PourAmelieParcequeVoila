d863fa70d096db32be56b588c0208bf390941cfb
Amélie Cordier <acordier@gmail.com>
2016-05-28 11:49:29 +0200

    Javadoc + format dans HumanPlayer

--- src/model/HumanPlayer.java
+++ src/model/HumanPlayer.java
@@ -1,15 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * MagicP4
+ * IUT Lyon 1 - 2016
  */
 package model;
 
 import java.awt.Color;
 
 /**
- *
- * @author hakkahi
+ * Class HumanPlayer
+ * @author hakkahi - IUT Lyon 1 - 2016
  */
 public class HumanPlayer extends Player {
 
