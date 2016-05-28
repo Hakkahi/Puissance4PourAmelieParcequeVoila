bcba1b44e75c8febc04f9e855e1135fd9cb81e14
Amélie Cordier <acordier@gmail.com>
2016-05-28 17:09:38 +0200

    Javadoc + format + todo + refactoring dans ChangeColorEffect

--- src/model/ChangeColorEffect.java
+++ src/model/ChangeColorEffect.java
@@ -1,23 +1,37 @@
+/**
+ * MagicP4
+ * IUT Lyon 1 - 2016
+ */
 package model;
 
 /**
  *
- * @author hakkahi
- *
+ * @author hakkahi - IUT Lyon 1 - 2016
  */
 public class ChangeColorEffect extends Effect {
 
+    /**
+     * This effect changes the color of the tile that has just been played and
+     * thus the owner of the tile)
+     *
+     * @param line
+     * @param column
+     * @param game
+     */
     @Override
     public void playEffect(int line, int column, Game game) {
 
-        int id = game.getBoard().getTileIJ(line, column).getIdPlayer();
+        int tile_id = game.getBoard().getTileIJ(line, column).getStatus();
+        int player1_id = game.getPlayer1().getId();
+        int player2_id = game.getPlayer2().getId();
 
-        if (id == game.getPlayer1().getId()) {
-            game.getBoard().getTileIJ(line, column).setIdPlayer(game.getPlayer2().getId());
+        if (tile_id == player1_id) {
+            tile_id = player2_id;
         } else {
-            game.getBoard().getTileIJ(line, column).setIdPlayer(game.getPlayer1().getId());
+            tile_id = player2_id;
         }
-
+        game.getBoard().getTileIJ(line, column).setStatus(tile_id);
     }
+    //TODO : je me demande si c'est une bonne idée de passer les coordonnées dans l'effet. Ce serait pas plus simple de le faire sur une case ?  
 
 }
