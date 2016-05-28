d6e9debf3126bc2738ade3d508b8e24ea1928d91
Amélie Cordier <acordier@gmail.com>
2016-05-28 15:22:30 +0200

    Javadoc + format + todo dans EffectFactory

--- src/model/EffectFactory.java
+++ src/model/EffectFactory.java
@@ -1,17 +1,27 @@
+/**
+ * MagicP4
+ * IUT Lyon 1 - 2016
+ */
 package model;
 
 import java.util.Random;
 
 /**
+ * Class EffectFactory
  *
- * @author hakkahi
+ * @author hakkahi - IUT Lyon 1 - 2016
  *
  */
 public class EffectFactory {
 
+    /**
+     * Returns a random effect chosen amongst the available effects listed
+     *
+     * @return an effect
+     */
     public static Effect createEffect() {
         int min = 0;
-        int max = 0;
+        int max = 1;
         Random rand = new Random();
         //Tire un nombre aléatoire entre min et max compris
         int random = rand.nextInt(max - min + 1) + min;
@@ -21,11 +31,12 @@ public class EffectFactory {
             case 0:
                 return new ChangeColorEffect();
             case 1:
-            //return new DisappearEffect();
+                return new DisappearEffect();
         }
 
         return null;
 
     }
+    //TODO : ce serait chouette que le random soit paramétrable dans la méthode aussi 
 
 }
